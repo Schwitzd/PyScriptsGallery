dfc4c0a8bc34e0a42fa4998eea91824c49d7e0a2
Schwitzd <schwitzd@gmail.com>
2021-01-12 19:43:24 +0100

    added OutTxtTable.py

---
+++ OutTxtTable.py
@@ -0,0 +1,15 @@
+import argparse
+from prettytable import from_csv
+
+parser = argparse.ArgumentParser()
+parser.add_argument(--csvfile', type=argparse.FileType('r'), help="CSV file path")
+args = parser.parse_args()
+
+def main():
+    with args.csvfile as csvfile:
+        output = from_csv(csvfile)
+
+    print(output)
+
+if __name__ == "__main__":
+    main()
