1354407317932ce79bc77b479c30cca64a0ffefd
Schwitzd <schwitzd@gmail.com>
2021-12-22 09:10:51 +0100

    code improved

--- OutTxtTable.py
+++ OutTxtTable.py
@@ -1,15 +1,26 @@
 import argparse
 from prettytable import from_csv
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--csvfile', type=argparse.FileType('r'), help="CSV file path")
-args = parser.parse_args()
+def get_arguments():
+    '''Define the input arguments'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--csvfile', dest='csvfile', type=argparse.FileType('r'), help='CSV file path')
+    args = parser.parse_args()
+
+    if not args.csvfile:
+        parser.error("[-] Please specify a csv file, use --help for more info.")
+
+    return args
+
 
 def main():
+    '''Main function'''
+    args = get_arguments()
     with args.csvfile as csvfile:
         output = from_csv(csvfile)
 
     print(output)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
